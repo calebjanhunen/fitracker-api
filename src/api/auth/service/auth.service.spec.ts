5b4cbac6ca1c5b67de4b3d4ca156fde1010ae349
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-10-07 21:52:32 -0400

    FIT-89: Add signin function in AuthService

--- src/api/auth/service/auth.service.spec.ts
+++ src/api/auth/service/auth.service.spec.ts
@@ -1,18 +1,90 @@
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from 'src/api/user/service/user.service';
+import { SkillLevel } from 'src/api/utils/enums/skill-level';
+import * as passwordHelper from 'src/api/utils/helpers/password-helper';
+import { User } from 'src/model/user.entity';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UserService;
+  let jwtServiceMock: JwtService;
+
+  const testUser: User = {
+    id: 'test-uuid',
+    username: 'testuser',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    firstName: 'Test',
+    lastName: 'User',
+    password: '123',
+    email: 'test@example.com',
+    skillLevel: SkillLevel.advanced,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: UserService,
+          useValue: {
+            getByUsername: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtServiceMock = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(jwtServiceMock).toBeDefined();
+  });
+
+  describe('Test signIn()', () => {
+    const username = 'testuser';
+    const password = '1234';
+
+    it('should throw NotFoundException if user does not exist', async () => {
+      jest.spyOn(userService, 'getByUsername').mockResolvedValueOnce(null);
+
+      const signin = async () => {
+        await authService.signIn(username, password);
+      };
+
+      expect(signin).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw UnauthorizedException if passwords do not match', async () => {
+      jest.spyOn(userService, 'getByUsername').mockResolvedValueOnce(testUser);
+
+      const signin = async () => {
+        await authService.signIn(username, password);
+      };
+
+      expect(signin).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should return jwt access token if successfully logged in', async () => {
+      jest.spyOn(userService, 'getByUsername').mockResolvedValueOnce(testUser);
+      jest
+        .spyOn(passwordHelper, 'comparePasswords')
+        .mockResolvedValueOnce(true);
+      jest
+        .spyOn(jwtServiceMock, 'signAsync')
+        .mockResolvedValue('test-access-token');
+
+      const result = await authService.signIn(username, '123');
+
+      expect(result).toBe('test-access-token');
+    });
   });
 });
