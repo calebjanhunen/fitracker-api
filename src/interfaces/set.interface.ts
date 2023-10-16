6130cd4f8a5226c691c6374a86bb705bc5e44826
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-10-16 16:27:41 -0400

    FIT-96: Add base interface

--- src/interfaces/set.interface.ts
+++ src/interfaces/set.interface.ts
@@ -1,8 +1,8 @@
+import { Base } from './base.interface';
 import { IExercise } from './exercise.interface';
 import { IWorkout } from './workout.interface';
 
-export interface ISet {
-  id: string;
+export interface ISet extends Base {
   weight: number;
   reps: number;
   rpe: number;
--- src/interfaces/workout.interface.ts
+++ src/interfaces/workout.interface.ts
@@ -1,10 +1,8 @@
+import { Base } from './base.interface';
 import { IExercise } from './exercise.interface';
 import { IUser } from './user.interface';
 
-export interface IWorkout {
-  id?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+export interface IWorkout extends Base {
   user: IUser;
   name: string;
   exercises: IExercise[];
