eacd58eb4a5fc11c8a297a80060e8531db74890a
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-11-17 10:59:49 -0500

    FIT-106: Add hasMore field to list response

--- src/model/collection.model.ts
+++ src/model/collection.model.ts
@@ -1,4 +1,15 @@
 export class CollectionModel<T> {
   public listObjects: T[];
   public totalCount: number;
+  public offset: number;
+  public limit: number;
+
+  /**
+   * Returns true if there are more records available and false if not
+   *
+   * @returns {boolean}
+   */
+  public hasMore(): boolean {
+    return this.limit + this.offset < this.totalCount ? true : false;
+  }
 }
