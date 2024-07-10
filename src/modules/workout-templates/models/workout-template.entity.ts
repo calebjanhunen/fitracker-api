7ae1da84e0e88e98a8151b1993f8178d64682afb
Caleb Janhunen <calebjanhunen@gmail.com>
2024-07-10 18:03:49 -0400

    Add database models

---
+++ src/modules/workout-templates/models/workout-template.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity } from 'src/model/base.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { WorkoutTemplateExercise } from './workout-template-exercise.entity';
+
+@Entity('workout_templates')
+export class WorkoutTemplate extends BaseEntity {
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  name: string;
+
+  @OneToMany(
+    () => WorkoutTemplateExercise,
+    (WorkoutTemplateExercise) => WorkoutTemplateExercise.workout,
+  )
+  workoutTemplateExercises: WorkoutTemplateExercise[];
+}
