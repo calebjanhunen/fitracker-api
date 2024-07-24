3417eb7bfc408de6a28359edc0a93521767504df
Caleb Janhunen <calebjanhunen@gmail.com>
2024-07-24 11:25:06 -0400

    Add more tests for findById

--- src/modules/workout-templates/repository/tests/workout-template-repository-findById.int-spec.ts
+++ src/modules/workout-templates/repository/tests/workout-template-repository-findById.int-spec.ts
@@ -1,15 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import {
-  clearDatabase,
-  loadDataFromJsonIntoDb,
-} from 'src/scripts/load-data-into-test-db';
-import { integrationDataSource } from 'test/integration/integration-test-datasource';
-import { IntegrationTestModule } from 'test/integration/jest-integration.module';
+import { TestingModule } from '@nestjs/testing';
 import {
   setupTestEnvironment,
   teardownTestEnvironment,
 } from 'test/utils/integration-environment-setup';
-import { DataSource } from 'typeorm';
 import { WorkoutTemplateRepository } from '../workout-template.repository';
 
 describe('findById()', () => {
@@ -39,5 +32,20 @@ describe('findById()', () => {
     expect(workoutTemplate?.id).toBe('81622181-0768-45e1-943f-86099cd91961');
     expect(workoutTemplate?.workoutTemplateExercises.length).toBe(1);
     expect(workoutTemplate?.workoutTemplateExercises[0].sets.length).toBe(1);
+  });
+  it('should be null if workout template with id does not exist', async () => {
+    const nonExistentId = '7c91be23-18d1-4bc9-ab5f-4a2359d316b7';
+    const workoutTemplate = await workoutTemplateRepo.findById(
+      nonExistentId,
+      '4e06c1c0-a0d6-4f0c-be74-4eb8678a70e8',
+    );
+    expect(workoutTemplate).toBeNull();
+  });
+  it('should be null if workout template exists but does not belong to user', async () => {
+    const workoutTemplate = await workoutTemplateRepo.findById(
+      '81622181-0768-45e1-943f-86099cd91961',
+      'f9610720-0b25-4e1f-8d09-714b5ffaef4b',
+    );
+    expect(workoutTemplate).toBeNull();
   });
 });
