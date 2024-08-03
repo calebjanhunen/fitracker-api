db173979e042433bf6839517f0a0304520d1ef66
Caleb Janhunen <calebjanhunen@gmail.com>
2024-08-03 12:42:16 -0400

    Add update workout template repository method

--- src/modules/workout-templates/repository/workout-template.repository.ts
+++ src/modules/workout-templates/repository/workout-template.repository.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseRepository } from 'src/common/repository/base.repository';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { WorkoutTemplateExercise } from '../models/workout-template-exercise.entity';
+import { WorkoutTemplateSet } from '../models/workout-template-set.entity';
 import { WorkoutTemplate } from '../models/workout-template.entity';
 
 @Injectable()
@@ -9,6 +11,7 @@ export class WorkoutTemplateRepository extends BaseRepository<WorkoutTemplate> {
   constructor(
     @InjectRepository(WorkoutTemplate)
     private repo: Repository<WorkoutTemplate>,
+    private dataSource: DataSource,
   ) {
     super(repo);
   }
@@ -48,7 +51,104 @@ export class WorkoutTemplateRepository extends BaseRepository<WorkoutTemplate> {
     await this.repo.remove(entity);
   }
 
-  public async update(entity: WorkoutTemplate): Promise<WorkoutTemplate> {
-    return await this.repo.save(entity);
+  public async update(
+    entity: WorkoutTemplate,
+    existingEntity: WorkoutTemplate,
+    userId: string,
+  ): Promise<WorkoutTemplate | null> {
+    // update workout template entity (name)
+    /*
+    update workout template exercises
+      insert any new workoutTemplateExercises that are in the updatedEntity bu arent in the existingEntity
+      delete any workoutTemplateExercises that are in the existingEntity but arent in the updatedEntity
+      update existing workoutTemplateExercises that are both in the updatedEntity and existingEntity (order)
+
+      update workoutTemplateSets for each exercise
+        insert any new workoutTemplateSets
+        delete any workoutTemplateSets not in updatedEntity
+        update existing sets (type, order)
+    */
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager
+        .createQueryBuilder()
+        .update(WorkoutTemplate)
+        .set({ name: entity.name })
+        .where('id = :id', { id: existingEntity.id })
+        .execute();
+
+      for (const workoutTemplateExercise of entity.workoutTemplateExercises) {
+        if (!workoutTemplateExercise.id) {
+          // Create workout template exercise
+          await queryRunner.manager.save(workoutTemplateExercise);
+        } else {
+          // Update workout template exercise
+          await queryRunner.manager
+            .createQueryBuilder()
+            .update(WorkoutTemplateExercise)
+            .set({
+              order: workoutTemplateExercise.order,
+            })
+            .where('id = :id', { id: workoutTemplateExercise.id })
+            .execute();
+
+          for (const workoutTemplateSet of workoutTemplateExercise.sets) {
+            if (!workoutTemplateSet.id) {
+              // Create workout template set
+              await queryRunner.manager.save(workoutTemplateSet);
+            } else {
+              // Update workout template set
+              await queryRunner.manager
+                .createQueryBuilder()
+                .update(WorkoutTemplateSet)
+                .set({
+                  order: workoutTemplateSet.order,
+                  type: workoutTemplateSet.type,
+                })
+                .where('id = :id', { id: workoutTemplateSet.id })
+                .execute();
+            }
+          }
+
+          // delete sets that are in existing workout template exercise but not in updatedEntity exercise
+          const existingWorkoutTemplateExercise =
+            existingEntity.workoutTemplateExercises.find(
+              (e) => e.id === workoutTemplateExercise.id,
+            )!;
+          const workoutTemplateSetIds = workoutTemplateExercise.sets.map(
+            (set) => set.id,
+          );
+          for (const existingWorkoutTemplateSet of existingWorkoutTemplateExercise.sets) {
+            if (
+              !workoutTemplateSetIds.includes(existingWorkoutTemplateSet.id)
+            ) {
+              await queryRunner.manager.remove(existingWorkoutTemplateSet);
+            }
+          }
+        }
+      }
+
+      // Delete exercises in existingWorkoutTemplate but not in updatedEntity
+      const workoutTemplateExerciseIds = entity.workoutTemplateExercises.map(
+        (e) => e.id,
+      );
+      for (const existingWorkoutExercise of existingEntity.workoutTemplateExercises) {
+        if (!workoutTemplateExerciseIds.includes(existingWorkoutExercise.id)) {
+          await queryRunner.manager.remove(existingWorkoutExercise);
+        }
+      }
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      if (queryRunner.isTransactionActive)
+        await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
+    return await this.findById(existingEntity.id, userId);
   }
 }
--- test/utils/integration-environment-setup.ts
+++ test/utils/integration-environment-setup.ts
@@ -6,13 +6,14 @@ import {
 import { getIntegrationDataSourceInstance } from 'test/integration/integration-test-datasource';
 import { IntegrationTestModule } from 'test/integration/jest-integration.module';
 
-export async function setupTestEnvironment(pathToJsonFile: string) {
-  const dataSourceInstance = await getIntegrationDataSourceInstance();
+let pathToJsonFile: string;
+
+export async function setupTestEnvironment(path: string) {
+  pathToJsonFile = path;
   const module = await Test.createTestingModule({
     imports: [IntegrationTestModule],
   }).compile();
 
-  await loadDataFromJsonIntoDb(pathToJsonFile, dataSourceInstance);
   return module;
 }
 
@@ -25,5 +26,15 @@ export async function teardownTestEnvironment(module: TestingModule) {
     await module.close();
   } catch (e) {
     console.error('Failed to teardown environment: ', e);
+  }
+}
+
+export async function resetDatabase() {
+  const dataSourceInstance = await getIntegrationDataSourceInstance();
+  try {
+    clearDatabase(dataSourceInstance);
+    await loadDataFromJsonIntoDb(pathToJsonFile, dataSourceInstance);
+  } catch (e) {
+    console.error('Could not reset database: ', e);
   }
 }
