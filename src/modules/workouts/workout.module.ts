e5ae57dd523594f7e760e7d865252c6446b642f7
Caleb Janhunen <calebjanhunen@gmail.com>
2024-09-04 11:00:28 -0400

    Add unit tests for create method in workoutservice

--- src/modules/workouts/workout.module.ts
+++ src/modules/workouts/workout.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from 'src/common/database/database.module';
 import { ExerciseModule } from '../exercises/exercises.module';
-import { UserModule } from '../user/user.module';
 import { WorkoutController } from './controller/workout.controller';
 import { WorkoutRepository } from './repository/workout.repository';
 import { WorkoutService } from './service/workout.service';
 
 @Module({
-  imports: [ExerciseModule, UserModule, DbModule],
+  imports: [ExerciseModule, DbModule],
   providers: [WorkoutService, WorkoutRepository],
   controllers: [WorkoutController],
   exports: [WorkoutService],
