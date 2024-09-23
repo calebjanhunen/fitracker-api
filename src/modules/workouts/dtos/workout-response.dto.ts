0302dbfeea90d58cf19c9b6a2ef5a9621d1a4177
Caleb Janhunen <calebjanhunen@gmail.com>
2024-09-22 22:16:56 -0400

    Hide gainedXp from dto

--- src/modules/workouts/dtos/workout-response.dto.ts
+++ src/modules/workouts/dtos/workout-response.dto.ts
@@ -1,22 +1,49 @@
+import { Expose } from 'class-transformer';
+
 export class WorkoutResponseDto {
+  @Expose()
   id: string;
+
+  @Expose()
   name: string;
+
+  @Expose()
   createdAt: Date;
+
+  @Expose()
   duration: number;
+
+  @Expose()
   exercises: WorkoutExerciseResponseDto[];
 }
 
 export class WorkoutExerciseResponseDto {
+  @Expose()
   id: string;
+
+  @Expose()
   name: string;
+
+  @Expose()
   order: number;
+
+  @Expose()
   sets: WorkoutSetResponseDto[];
 }
 
 export class WorkoutSetResponseDto {
+  @Expose()
   id: string;
+
+  @Expose()
   order: number;
+
+  @Expose()
   weight: number;
+
+  @Expose()
   reps: number;
+
+  @Expose()
   rpe: number;
 }
