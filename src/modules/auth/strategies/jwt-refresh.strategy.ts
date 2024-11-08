06ae017d2484c0682dabf7c8f1b3c5150a735cad
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-08 12:50:46 -0500

    Add safety checks to ensure refresh token and device id are in header

--- src/modules/auth/strategies/jwt-refresh.strategy.ts
+++ src/modules/auth/strategies/jwt-refresh.strategy.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { LoggerServiceV2 } from 'src/common/logger/logger-v2.service';
 import { AuthService } from '../service/auth.service';
 
 @Injectable()
@@ -13,19 +14,41 @@ export class JwtRefreshStrategy extends PassportStrategy(
   constructor(
     configService: ConfigService,
     private authService: AuthService,
+    private logger: LoggerServiceV2,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromHeader('x-refresh-token'),
       secretOrKey: configService.getOrThrow<string>('REFRESH_TOKEN_SECRET'),
       passReqToCallback: true,
     });
+    this.logger.setContext(JwtRefreshStrategy.name);
   }
   async validate(request: Request, payload: { userId: string }) {
     try {
+      const deviceId = request.headers['x-device-id'] as string;
+      if (!deviceId) {
+        // This should never happen
+        this.logger.error(
+          'Request header did not contain device id',
+          new Error('No device id in request header'),
+        );
+        throw new Error();
+      }
+
+      const refreshToken = request.headers['x-refresh-token'] as string;
+      if (!refreshToken) {
+        // This should never happen
+        this.logger.error(
+          'Request header did not contain refresh token',
+          new Error('No refresh token in request header'),
+        );
+        throw new Error();
+      }
+
       return await this.authService.verifyRefreshToken(
-        request.headers['x-refresh-token'] as string,
+        refreshToken,
         payload.userId,
-        request.headers['x-device-id'] as string,
+        deviceId,
       );
     } catch (e) {
       throw new UnauthorizedException(e);
