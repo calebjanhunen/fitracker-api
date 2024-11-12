b05b67ace3416213a128dba55bac3fc9f77f8442
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-11 22:19:26 -0500

    Don't send email in development environment

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,5 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { LoggerServiceV2 } from 'src/common/logger/logger-v2.service';
 import { MailFailedToSendException } from './internal-exceptions/mail-failed-to-send.exception';
 
@@ -8,6 +9,7 @@ export class MailService {
   constructor(
     private readonly mailerService: MailerService,
     private readonly logger: LoggerServiceV2,
+    private readonly configService: ConfigService,
   ) {
     this.logger.setContext(MailService.name);
   }
@@ -15,22 +17,24 @@ export class MailService {
   public async sendSignupCode(
     emailAddress: string,
     code: string,
-  ): Promise<string> {
+  ): Promise<void> {
     try {
-      const result = await this.mailerService.sendMail({
-        to: emailAddress,
-        subject: 'Welcome to Fitracker!',
-        template: './signup-code',
-        context: {
-          code,
-        },
-      });
-      this.logger.log(`Signup email successfully sent: ${result.response}`);
+      if (this.configService.getOrThrow('NODE_ENV') !== 'development') {
+        const result = await this.mailerService.sendMail({
+          to: emailAddress,
+          subject: 'Welcome to Fitracker!',
+          template: './signup-code',
+          context: {
+            code,
+          },
+        });
+        this.logger.log(`Signup email successfully sent: ${result.response}`);
+      } else {
+        this.logger.log('In development environment. Email not sent');
+      }
     } catch (e) {
       this.logger.error('Signup Code email failed to send: ', e);
       throw new MailFailedToSendException(e);
     }
-
-    return code;
   }
 }
