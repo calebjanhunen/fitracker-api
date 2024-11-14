b0d76bd25ae5259200c16d683ee515dbdf158372
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-14 12:56:24 -0500

    Send email in UserNotValidatedException

--- src/modules/auth/internal-exceptions/user-is-not-validated.exception.ts
+++ src/modules/auth/internal-exceptions/user-is-not-validated.exception.ts
@@ -1,7 +1,7 @@
 import { BaseException } from 'src/common/internal-exceptions/base.exception';
 
 export class UserIsNotValidatedException extends BaseException {
-  constructor() {
-    super('User is not validated');
+  constructor(email: string) {
+    super(email);
   }
 }
