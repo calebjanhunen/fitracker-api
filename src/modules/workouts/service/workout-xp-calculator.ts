5ee1426d9ab787559045ec747417719154c66ec5
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-01 20:18:15 -0500

    Add xp calculation for workout effort

--- src/modules/workouts/service/workout-xp-calculator.ts
+++ src/modules/workouts/service/workout-xp-calculator.ts
@@ -1,46 +1,194 @@
-import { InsertWorkoutModel } from '../models';
+import { InsertWorkoutModel, InsertWorkoutSetModel } from '../models';
 
 interface ICalculateWorkoutXp {
-  totalXpGained: number;
-  xpGainedFromWorkoutDuration: number;
+  totalXp: number;
+  workoutEffortXp: number;
 }
 
 export class WorkoutXpCalculator {
-  private readonly MIN_WORKOUT_DURATION_FOR_XP_MS = 60 * 1000 * 15; // 15 minutes
-  private readonly XP_FOR_EACH_WORKOUT_MINUTE = 1;
-  private readonly MS_TO_MIN_CONVERSION = 60000;
+  private readonly MS_TO_SECOND_CONVERSION = 1000;
+  private readonly MAX_RPE = 10;
+  private readonly MIN_RPE = 5;
+  private readonly SUGGESTED_MIN_TIME_PER_SET_SECONDS = 30;
+  private readonly SUGGESTED_MAX_TIME_PER_SET_SECONDS = 60;
+  private readonly SUGGESTED_MIN_TIME_REST_TIME_SECONDS = 60; // 1 minute
+  private readonly SUGGESTED_MAX_TIME_REST_TIME_SECONDS = 60 * 3; // 3 minutes
+  private readonly TRANSITION_TIME_BETWEEN_EXERCISES_SECONDS = 60 * 5; // 5 minutes
+  private readonly DURATION_FACTOR_DECAY_RATE = 0.02;
+  private readonly WORKOUT_EFFORT_XP_MULTIPLIER = 1;
   constructor() {}
 
-  public calculateWorkoutXp(workout: InsertWorkoutModel): ICalculateWorkoutXp {
-    const xpGainedFromWorkoutDuration =
-      this.calculateXpGainedFromWorkoutDuration(workout);
-
-    return {
-      totalXpGained: xpGainedFromWorkoutDuration,
-      xpGainedFromWorkoutDuration,
-    };
+  public calculateTotalXp(workout: InsertWorkoutModel): ICalculateWorkoutXp {
+    const workoutEffortXp = this.calculateWorkoutEffortXp(workout);
+    const totalXp = workoutEffortXp;
+    return { totalXp, workoutEffortXp };
   }
 
   /**
-   * Calculates xp gained from workout duration if workout is longer than 15 minutes
-   *    - Gives 1 xp for every minute of the workout
+   * Calculates the amount of xp gained from workout effort
    * @param {InsertWorkoutModel} workout
    * @returns {number}
    */
-  private calculateXpGainedFromWorkoutDuration(
+  private calculateWorkoutEffortXp(workout: InsertWorkoutModel): number {
+    const actualWorkoutDurationSeconds =
+      (workout.lastUpdatedAt.getTime() - workout.createdAt.getTime()) /
+      this.MS_TO_SECOND_CONVERSION;
+    console.log(actualWorkoutDurationSeconds);
+
+    const totalWorkoutEffort = this.calculateWorkoutEffortWithWorkoutDuration(
+      workout,
+      actualWorkoutDurationSeconds,
+    );
+
+    const workoutEffortXp =
+      totalWorkoutEffort * this.WORKOUT_EFFORT_XP_MULTIPLIER;
+
+    return workoutEffortXp;
+  }
+
+  /**
+   * Calculates the workout effort while taking the workout duration into account
+   * @param {InsertWorkoutModel} workout
+   * @param {number} workoutDurationSeconds
+   * @returns {number}
+   */
+  private calculateWorkoutEffortWithWorkoutDuration(
     workout: InsertWorkoutModel,
+    workoutDurationSeconds: number,
   ): number {
-    const actualWorkoutDuration =
-      workout.lastUpdatedAt.getTime() - workout.createdAt.getTime();
+    const { numExercises, numSets } =
+      this.getNumberOfExercisesAndSetsInWorkout(workout);
 
-    if (actualWorkoutDuration >= this.MIN_WORKOUT_DURATION_FOR_XP_MS) {
-      const workoutLengthInMinutes = Math.floor(
-        actualWorkoutDuration / this.MS_TO_MIN_CONVERSION,
+    const { minBaselineDurationSeconds, maxBaselineDurationSeconds } =
+      this.calculateBaselineWorkoutTimesInSeconds(numSets, numExercises);
+
+    let durationFactor: number;
+    if (
+      workoutDurationSeconds > minBaselineDurationSeconds &&
+      workoutDurationSeconds <= maxBaselineDurationSeconds
+    ) {
+      durationFactor = 1;
+    } else if (workoutDurationSeconds <= minBaselineDurationSeconds) {
+      durationFactor = Math.exp(
+        -this.DURATION_FACTOR_DECAY_RATE *
+          (minBaselineDurationSeconds - workoutDurationSeconds),
       );
-
-      return workoutLengthInMinutes * this.XP_FOR_EACH_WORKOUT_MINUTE;
     } else {
-      return 0;
+      durationFactor = Math.exp(
+        -this.DURATION_FACTOR_DECAY_RATE *
+          (workoutDurationSeconds - maxBaselineDurationSeconds),
+      );
     }
+
+    const workoutEffort = this.calculateWorkoutEffort(workout);
+
+    return workoutEffort * durationFactor;
+  }
+
+  /**
+   * Calculates the workout effort by summing all set efforts
+   * @param {InsertWorkoutModel} workout
+   * @returns {number}
+   */
+  private calculateWorkoutEffort(workout: InsertWorkoutModel): number {
+    let workoutEffort = 0;
+    for (const exercise of workout.exercises) {
+      let exerciseEffort = 0;
+      for (const set of exercise.sets) {
+        console.log(set, this.calculateSetEffort(set));
+        exerciseEffort += this.calculateSetEffort(set);
+      }
+      workoutEffort += exerciseEffort;
+    }
+
+    return workoutEffort;
+  }
+
+  /**
+   * Calculates a numerical value for the effort of a set
+   *  - Uses relative weight so a set with more weight doesn't automatically result in more effort
+   * @param {InsertWorkoutSetModel} set
+   * @returns {number}
+   */
+  private calculateSetEffort(set: InsertWorkoutSetModel): number {
+    const oneRepMaxInLbs = this.getOneRepMax(set);
+    const relativeWeight = set.weight / oneRepMaxInLbs;
+    const intensityFactor = set.rpe
+      ? (set.rpe - this.MIN_RPE) / this.MIN_RPE
+      : 1;
+    return relativeWeight * set.reps * intensityFactor;
+  }
+
+  /**
+   * Calculates the minimum and maximum duration a workout should be based on the number of sets and exercises
+   * @param {number} numSets
+   * @param {number} numExercises
+   * @returns {number}
+   */
+  private calculateBaselineWorkoutTimesInSeconds(
+    numSets: number,
+    numExercises: number,
+  ): {
+    minBaselineDurationSeconds: number;
+    maxBaselineDurationSeconds: number;
+  } {
+    // TODO: Find a way to more accurately get the min and max baseline times based on goals, exercise type, etc
+    const minBaselineDurationSeconds =
+      numSets * this.SUGGESTED_MIN_TIME_PER_SET_SECONDS +
+      numSets * this.SUGGESTED_MIN_TIME_REST_TIME_SECONDS +
+      (numExercises - 1) * this.TRANSITION_TIME_BETWEEN_EXERCISES_SECONDS;
+
+    const maxBaselineDurationSeconds =
+      numSets * this.SUGGESTED_MAX_TIME_PER_SET_SECONDS +
+      numSets * this.SUGGESTED_MAX_TIME_REST_TIME_SECONDS +
+      (numExercises - 1) * this.TRANSITION_TIME_BETWEEN_EXERCISES_SECONDS;
+
+    return { minBaselineDurationSeconds, maxBaselineDurationSeconds };
+  }
+
+  private getNumberOfExercisesAndSetsInWorkout(workout: InsertWorkoutModel): {
+    numExercises: number;
+    numSets: number;
+  } {
+    const numExercises = workout.exercises.length;
+    const numSets = workout.exercises.reduce((total, exercise) => {
+      return total + exercise.sets.length;
+    }, 0);
+
+    return { numExercises, numSets };
+  }
+
+  /**
+   * Calculates the one rep max for a set using the Epley formula
+   *  - Uses set reps + (MAX_RPE - set rpe) to get the maximum number of reps if the rpe was 10
+   * @param {InsertWorkoutSetModel} set
+   * @returns {number}
+   */
+  private getOneRepMax(set: InsertWorkoutSetModel): number {
+    const weightInKg = this.convertWeight(set.weight, 'lbs', 'kg');
+    const maxReps = set.rpe ? set.reps + (this.MAX_RPE - set.rpe) : set.reps;
+
+    const oneRepMaxInKg = weightInKg * (1 + maxReps / 30);
+    return Math.round(this.convertWeight(oneRepMaxInKg, 'kg', 'lbs'));
+  }
+
+  /**
+   * Converts the input weight from lbs to kg or vice versa
+   * @param {number} weight
+   * @param {'lbs' | 'kg'} originalUnit - Unit of the input weight
+   * @param {'lbs' | 'kg'} resultUnit - Unit of the desired weight
+   * @returns {number}
+   */
+  private convertWeight(
+    weight: number,
+    originalUnit: 'lbs' | 'kg',
+    resultUnit: 'lbs' | 'kg',
+  ): number {
+    if (originalUnit === 'lbs' && resultUnit === 'kg') {
+      return weight / 2.205;
+    } else if (originalUnit === 'kg' && resultUnit === 'lbs') {
+      return weight * 2.205;
+    }
+    return weight;
   }
 }
