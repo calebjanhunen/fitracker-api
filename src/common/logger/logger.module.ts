5789e49a17765e5152880610d39b2740a23b2622
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-10 23:26:00 -0500

    Setup logtail

--- src/common/logger/logger.module.ts
+++ src/common/logger/logger.module.ts
@@ -1,4 +1,5 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 import { v4 as uuidv4 } from 'uuid';
 import { LoggerService } from './logger.service';
@@ -6,16 +7,36 @@ import { LoggerService } from './logger.service';
 @Global()
 @Module({
   imports: [
-    PinoLoggerModule.forRoot({
-      pinoHttp: {
-        autoLogging: false,
-        genReqId: (request) => request.headers['x-correlation-id'] || uuidv4(),
-        redact: {
-          paths: [
-            'req.headers.authorization',
-            "req.headers['x-refresh-token']",
-          ],
-        },
+    PinoLoggerModule.forRootAsync({
+      providers: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          pinoHttp: {
+            autoLogging: false,
+            genReqId: (request) =>
+              request.headers['x-correlation-id'] || uuidv4(),
+            redact: {
+              paths: [
+                'req.headers.authorization',
+                "req.headers['x-refresh-token']",
+              ],
+            },
+            transport: {
+              targets: [
+                {
+                  target: '@logtail/pino',
+                  options: {
+                    sourceToken: configService.getOrThrow(
+                      'LOGTAIL_SOURCE_TOKEN',
+                    ),
+                  },
+                  worker: false,
+                },
+              ],
+            },
+          },
+        };
       },
     }),
   ],
