071c40eab2df12b102cb124238cdec11bfa86931
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-15 22:27:14 -0500

    Remove user refresh token

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,24 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { DbModule } from 'src/common/database/database.module';
 import { UserController } from './controller/user.controller';
-import { UserRefreshTokenReposistory } from './repository/user-refresh-token.repository';
 import { UserRepository } from './repository/user.repository';
 import { UserCronService } from './service/user-cron.service';
-import { UserRefreshTokenService } from './service/user-refresh-token.service';
 import { UserService } from './service/user.service';
 import { UserProfile } from './user.profile';
 
 @Module({
   imports: [DbModule],
   controllers: [UserController],
-  providers: [
-    UserService,
-    UserRepository,
-    UserProfile,
-    UserCronService,
-    UserRefreshTokenReposistory,
-    UserRefreshTokenService,
-  ],
-  exports: [UserService, UserRefreshTokenService],
+  providers: [UserService, UserRepository, UserProfile, UserCronService],
+  exports: [UserService],
 })
 export class UserModule {}
