fd1d1348585cd1772f5328d2f4db8d01b517448c
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-17 20:16:19 -0500

    Add automapper to body part controller

--- src/modules/exercises/controller/body-part.controller.ts
+++ src/modules/exercises/controller/body-part.controller.ts
@@ -1,19 +1,24 @@
+import { Mapper } from '@automapper/core';
+import { InjectMapper } from '@automapper/nestjs';
 import { ConflictException, Controller, Get, UseGuards } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { BodyPartDto } from '../dtos/body-part.dto';
+import { BodyPartModel } from '../models/body-part.model';
 import { BodyPartService } from '../services/body-part.service';
 
 @Controller('/api/body-parts')
 export class BodyPartController {
-  constructor(private readonly bodyPartService: BodyPartService) {}
+  constructor(
+    private readonly bodyPartService: BodyPartService,
+    @InjectMapper() private readonly mapper: Mapper,
+  ) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
   public async getAllBodyParts(): Promise<BodyPartDto[]> {
     try {
       const bodyParts = await this.bodyPartService.findAll();
-      return plainToInstance(BodyPartDto, bodyParts);
+      return this.mapper.mapArray(bodyParts, BodyPartModel, BodyPartDto);
     } catch (e) {
       throw new ConflictException(e.message);
     }
