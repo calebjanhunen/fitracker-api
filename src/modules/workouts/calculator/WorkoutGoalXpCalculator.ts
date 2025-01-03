6075f7c4a3feb91e5cfb3b405cd28329b3ec54c1
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-03 12:18:00 -0500

    Add WorkoutGoalXpCalculator and tests

--- src/modules/workouts/calculator/WorkoutGoalXpCalculator.ts
+++ src/modules/workouts/calculator/WorkoutGoalXpCalculator.ts
@@ -1,12 +1,31 @@
 export class WorkoutGoalXpCalculator {
   private readonly BASE_GOAL_XP = 25;
   private readonly GOAL_XP_MULTIPLIER = 5;
+  private readonly BONUS_XP_MULTIPLIER = 3;
   constructor() {}
 
-  public calculateWorkoutGoalXp(userWorkoutGoal: number) {
-    /**
-     * goal xp = base xp + (goal * multiplier)
-     */
+  public calculateWorkoutGoalXp(
+    userWorkoutGoal: number,
+    daysWithWorkoutsThisWeek: number,
+  ): number {
+    const workoutGoalBaseXp = this.calculateWorkoutGoalBaseXp(userWorkoutGoal);
+    const bonusXp = this.calculateBonusXp(
+      userWorkoutGoal,
+      daysWithWorkoutsThisWeek,
+    );
+    return workoutGoalBaseXp + bonusXp;
+  }
+
+  private calculateWorkoutGoalBaseXp(userWorkoutGoal: number): number {
     return this.BASE_GOAL_XP + userWorkoutGoal * this.GOAL_XP_MULTIPLIER;
+  }
+
+  private calculateBonusXp(
+    userWorkoutGoal: number,
+    daysWithWorkoutsThisWeek: number,
+  ): number {
+    return (
+      (daysWithWorkoutsThisWeek - userWorkoutGoal) * this.BONUS_XP_MULTIPLIER
+    );
   }
 }
--- test/unit/workout/calculator/WorkoutGoalXpCalculator.test.ts
+++ test/unit/workout/calculator/WorkoutGoalXpCalculator.test.ts
@@ -4,37 +4,60 @@ describe('WorkoutGoalXpCalculator', () => {
   const workoutGoalXpCalculator = new WorkoutGoalXpCalculator();
 
   describe('calculateWorkoutGoalXp', () => {
-    it('GivenWorkoutGoalOf3_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
+    it('GivenWorkoutGoalOf3AndDaysWithWorkoutsThisWeekEqualingWorkoutGoal_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
       const userWorkoutgoal = 3;
+      const daysWithWorkoutsThisWeek = 3;
 
-      const actual =
-        workoutGoalXpCalculator.calculateWorkoutGoalXp(userWorkoutgoal);
+      const actual = workoutGoalXpCalculator.calculateWorkoutGoalXp(
+        userWorkoutgoal,
+        daysWithWorkoutsThisWeek,
+      );
 
       expect(actual).toBe(40);
     });
-    it('GivenWorkoutGoalOf4_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
+    it('GivenWorkoutGoalOf4AndDaysWithWorkoutsThisWeekEqualingWorkoutGoal_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
       const userWorkoutgoal = 4;
+      const daysWithWorkoutsThisWeek = 4;
 
-      const actual =
-        workoutGoalXpCalculator.calculateWorkoutGoalXp(userWorkoutgoal);
+      const actual = workoutGoalXpCalculator.calculateWorkoutGoalXp(
+        userWorkoutgoal,
+        daysWithWorkoutsThisWeek,
+      );
 
       expect(actual).toBe(45);
     });
-    it('GivenWorkoutGoalOf5_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
+    it('GivenWorkoutGoalOf5AndDaysWithWorkoutsThisWeekEqualingWorkoutGoal_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
       const userWorkoutgoal = 5;
+      const daysWithWorkoutsThisWeek = 5;
 
-      const actual =
-        workoutGoalXpCalculator.calculateWorkoutGoalXp(userWorkoutgoal);
+      const actual = workoutGoalXpCalculator.calculateWorkoutGoalXp(
+        userWorkoutgoal,
+        daysWithWorkoutsThisWeek,
+      );
 
       expect(actual).toBe(50);
     });
-    it('GivenWorkoutGoalOf6_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
+    it('GivenWorkoutGoalOf6AndDaysWithWorkoutsThisWeekEqualingWorkoutGoal_WhenCalculatingWorkoutGoalXp_ReturnCorrectXpValue', () => {
       const userWorkoutgoal = 6;
+      const daysWithWorkoutsThisWeek = 6;
 
-      const actual =
-        workoutGoalXpCalculator.calculateWorkoutGoalXp(userWorkoutgoal);
+      const actual = workoutGoalXpCalculator.calculateWorkoutGoalXp(
+        userWorkoutgoal,
+        daysWithWorkoutsThisWeek,
+      );
 
       expect(actual).toBe(55);
+    });
+    it('GivenDaysWithWorkoutsThisWeekGreaterThanGoal_WhenCalculatingWOrkoutGoalXp_ReturnCorrectXpValue', () => {
+      const userWorkoutgoal = 6;
+      const daysWithWorkoutsThisWeek = 8;
+
+      const actual = workoutGoalXpCalculator.calculateWorkoutGoalXp(
+        userWorkoutgoal,
+        daysWithWorkoutsThisWeek,
+      );
+
+      expect(actual).toBe(61);
     });
   });
 });
