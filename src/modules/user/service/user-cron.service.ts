3f5afddba53689707c5c93918be5a0a290de11b0
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-07 12:02:01 -0500

    Add cron job to reset weekly workout goal streak

--- src/modules/user/service/user-cron.service.ts
+++ src/modules/user/service/user-cron.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { LoggerService } from 'src/common/logger/logger.service';
 import { UserRepository } from '../repository/user.repository';
 
@@ -12,12 +12,48 @@ export class UserCronService {
     this.logger.setContext(UserCronService.name);
   }
 
-  @Cron('0 0 * * 0', {
-    timeZone: 'UTC',
-  })
-  public async handleResetUserWeeklyWorkoutGoalStreak() {
-    this.logger.log(
-      `Cron job 'handleResetUserWeeklyWorkoutGoalStreak' is not implemented yet.`,
-    );
+  @Cron(CronExpression.EVERY_WEEK, { timeZone: 'UTC' })
+  public async resetUserWeeklyWorkoutGoalStreak() {
+    const cronJob = 'resetUserWeeklyWorkoutGoalStreak';
+    this.logger.log(`Start cron job ${cronJob}`, { cronJob });
+
+    try {
+      const startTime = Date.now();
+      const allUserStats = await this.userRepo.getStatsForAllUsers();
+      const todayAtMidnight = this.getTodayAtMidnight();
+
+      for (const userStat of allUserStats) {
+        if (!userStat.weeklyWorkoutGoalAchievedAt) {
+          continue;
+        }
+        if (
+          !userStat.weeklyWorkoutGoalAchievedAt.isWithinSevenDaysFrom(
+            todayAtMidnight,
+          )
+        ) {
+          userStat.weeklyWorkoutGoalStreak = 0;
+          await this.userRepo.updateUserStats(userStat, userStat.userId);
+
+          this.logger.log(
+            `Reset weekly workout goal streak for user ${userStat.userId}`,
+            { userId: userStat.userId },
+          );
+        }
+      }
+
+      const endTime = Date.now();
+      const elapsedTimeMs = endTime - startTime;
+      this.logger.log(`Finish cron job ${cronJob} in ${elapsedTimeMs}ms`, {
+        cronJob,
+        elapsedTimeMs,
+      });
+    } catch (e) {
+      this.logger.error(e, `Cron job ${cronJob} failed`, { cronJob });
+    }
+  }
+
+  private getTodayAtMidnight(): Date {
+    const timestamp = new Date().setHours(0, 0, 0, 0);
+    return new Date(timestamp);
   }
 }
