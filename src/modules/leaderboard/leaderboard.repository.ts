6d8d4e2a6c6dca938584f54240914e1113289455
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-13 21:54:40 -0500

    Refactor leaderboard repository to include user stats in total XP query and add error handling

--- src/modules/leaderboard/leaderboard.repository.ts
+++ src/modules/leaderboard/leaderboard.repository.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from 'src/common/database';
+import { DatabaseException } from 'src/common/internal-exceptions/database.exception';
 import { LoggerService } from 'src/common/logger/logger.service';
-import { TotalXpLeaderboardUser } from './models/total-xp-leaderboard-user.model';
+import { TotalXpLeaderboardUser } from './models';
 
 @Injectable()
 export class LeaderboardRepository {
@@ -13,10 +14,25 @@ export class LeaderboardRepository {
   public async getTotalXpLeaderboard(): Promise<TotalXpLeaderboardUser[]> {
     const queryName = 'getTotalXpLeaderboard';
     const query = `
-        SELECT
-            up.username,
-            up.total_xp
-        FROM public.user_profile
+      SELECT
+          up.username,
+          us.total_xp
+      FROM
+          public.user_profile up
+          LEFT JOIN public.user_stats us ON us.user_id = up.id
+      ORDER BY
+          us.total_xp DESC
     `;
+
+    try {
+      const { queryResult } = await this.db.queryV2<TotalXpLeaderboardUser>(
+        query,
+        [],
+      );
+      return queryResult;
+    } catch (e) {
+      this.logger.error(e, `Query ${queryName} failed`, { queryName });
+      throw new DatabaseException(e);
+    }
   }
 }
