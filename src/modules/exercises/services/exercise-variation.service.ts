02af37380add294b670d4df44d5566e74f8721c0
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-28 13:15:19 -0500

    Implement createExerciseVariation method in ExerciseVariationService with validation for exercise and cable attachment

--- src/modules/exercises/services/exercise-variation.service.ts
+++ src/modules/exercises/services/exercise-variation.service.ts
@@ -1,12 +1,52 @@
 import { Injectable } from '@nestjs/common';
-import { ExerciseVariationModel } from '../models';
-import { ExerciseVariationRepository } from '../repository/exercise-variation.repository';
+import { ResourceNotFoundException } from 'src/common/internal-exceptions';
+import {
+  CreateExerciseVariationModel,
+  ExerciseVariationModel,
+} from '../models';
+import {
+  CableAttachmentRepository,
+  ExerciseVariationRepository,
+} from '../repository';
+import { ExerciseService } from './exercise.service';
 
 @Injectable()
 export class ExerciseVariationService {
   constructor(
     private readonly exerciseVariationRepo: ExerciseVariationRepository,
+    private readonly cableAttachmentRepo: CableAttachmentRepository,
+    private readonly exerciseService: ExerciseService,
   ) {}
+
+  public async createExerciseVariation(
+    exerciseId: string,
+    userId: string,
+    model: CreateExerciseVariationModel,
+  ) {
+    await this.exerciseService.ensureExerciseExists(exerciseId, userId);
+
+    if (model.cableAttachmentId) {
+      const cableAttachment = await this.cableAttachmentRepo.getAttachmentById(
+        model.cableAttachmentId,
+      );
+
+      if (!cableAttachment) {
+        throw new ResourceNotFoundException('Cable attachment does not exist');
+      }
+    }
+
+    const exerciseVariationId =
+      await this.exerciseVariationRepo.createExerciseVariation(
+        exerciseId,
+        userId,
+        model,
+      );
+
+    return await this.exerciseVariationRepo.getExerciseVariationById(
+      exerciseVariationId,
+      userId,
+    )!;
+  }
 
   public async getExerciseVariationsByIds(
     exerciseVariationIds: string[],
