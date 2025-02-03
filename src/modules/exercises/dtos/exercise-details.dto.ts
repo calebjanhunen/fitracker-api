8af0cfae82a55700028aeefc4d72dac2064d11d4
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-03 13:03:17 -0500

    Add exerciseVariations property to ExerciseDetailsDto

--- src/modules/exercises/dtos/exercise-details.dto.ts
+++ src/modules/exercises/dtos/exercise-details.dto.ts
@@ -1,24 +1,34 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
+import { ExerciseVariationDto } from './exercise-variation.dto';
 import { ExerciseWorkoutHistoryDto } from './exercise-workout-history.dto';
 
 export class ExerciseDetailsDto {
   @AutoMap()
   @ApiProperty()
   public id: string;
+
   @AutoMap()
   @ApiProperty()
   public name: string;
+
   @AutoMap()
   @ApiProperty()
   public bodyPart: string;
+
   @AutoMap()
   @ApiProperty()
   public equipment: string;
+
   @AutoMap()
   @ApiProperty()
   public isCustom: boolean;
+
   @AutoMap(() => [ExerciseWorkoutHistoryDto])
   @ApiProperty({ type: ExerciseWorkoutHistoryDto, isArray: true })
   public workoutHistory: ExerciseWorkoutHistoryDto[];
+
+  @AutoMap(() => ExerciseVariationDto)
+  @ApiProperty({ type: ExerciseVariationDto, isArray: true })
+  public exerciseVariations: ExerciseVariationDto[];
 }
