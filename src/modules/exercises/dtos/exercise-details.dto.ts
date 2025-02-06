0497296e47af69c84d6760732dc68808f90b8969
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-06 12:12:52 -0500

    Add exercise type and parent exercise properties to exercise details and response DTOs

--- src/modules/exercises/dtos/exercise-details.dto.ts
+++ src/modules/exercises/dtos/exercise-details.dto.ts
@@ -1,5 +1,6 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
+import { ExerciseType } from '../enums/exercise-type.enum';
 import { ExerciseVariationDto } from './exercise-variation.dto';
 import { ExerciseWorkoutHistoryDto } from './exercise-workout-history.dto';
 
@@ -23,6 +24,18 @@ export class ExerciseDetailsDto {
   @AutoMap()
   @ApiProperty()
   public isCustom: boolean;
+
+  @AutoMap()
+  @ApiProperty({ type: 'string' })
+  public parentExerciseId?: string;
+
+  @AutoMap()
+  @ApiProperty({ type: 'string' })
+  public parentExerciseName?: string;
+
+  @AutoMap()
+  @ApiProperty({ enum: ExerciseType, required: true })
+  public exerciseType: ExerciseType;
 
   @AutoMap(() => [ExerciseWorkoutHistoryDto])
   @ApiProperty({ type: ExerciseWorkoutHistoryDto, isArray: true })
--- src/modules/exercises/dtos/exercise-response.dto.ts
+++ src/modules/exercises/dtos/exercise-response.dto.ts
@@ -1,14 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ExerciseType } from '../enums/exercise-type.enum';
 
 export class ExerciseResponseDto {
   @ApiProperty()
   public id: string;
+
   @ApiProperty()
   public name: string;
+
   @ApiProperty()
   public equipment: string;
+
   @ApiProperty()
   public bodyPart: string;
+
   @ApiProperty()
   public isCustom: boolean;
+
+  @ApiProperty({ enum: ExerciseType, required: true })
+  public exerciseType: ExerciseType;
+
+  @ApiProperty({ type: 'string' })
+  public parentExerciseId?: string;
 }
