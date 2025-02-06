7f5ee7d92f2faf934069182db4d3d47512a55f75
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-06 12:11:00 -0500

    Add methods to retrieve exercise variations and their workout history

--- src/modules/exercises/repository/exercise-variation.repository.ts
+++ src/modules/exercises/repository/exercise-variation.repository.ts
@@ -4,7 +4,9 @@ import { LoggerService } from 'src/common/logger/logger.service';
 import { BaseRepository } from 'src/common/repository';
 import {
   CreateExerciseVariationModel,
+  ExerciseModel,
   ExerciseVariationModel,
+  ExerciseWorkoutHistoryModel,
 } from '../models';
 
 @Injectable()
@@ -53,6 +55,118 @@ export class ExerciseVariationRepository extends BaseRepository {
       );
 
       return queryResult[0].id;
+    } catch (e) {
+      throw this.handleError(e, queryName);
+    }
+  }
+
+  public async getExerciseVariations(userId: string): Promise<ExerciseModel[]> {
+    const queryName = 'getExerciseVariationsForExerciseList';
+    const query = `
+      SELECT
+        ev.id,
+        ev.name,
+        bp.name as body_part,
+        eq.name as equipment,
+        true as is_custom,
+        e.id as parent_exercise_id
+      FROM public.exercise_variation ev
+      INNER JOIN exercise e ON e.id = ev.exercise_id
+      INNER JOIN body_part bp ON bp.id = e.body_part_id
+      INNER JOIN equipment eq ON eq.id = e.equipment_id
+      WHERE ev.user_id = $1
+    `;
+    const params = [userId];
+
+    try {
+      const { queryResult } = await this.db.queryV2<ExerciseModel>(
+        query,
+        params,
+      );
+
+      return queryResult;
+    } catch (e) {
+      throw this.handleError(e, queryName);
+    }
+  }
+
+  public async getExerciseVariationByIdV2(
+    exerciseVariationId: string,
+    userId: string,
+  ): Promise<ExerciseModel | null> {
+    const queryName = 'getExerciseVariationDetails';
+    const query = `
+      SELECT
+        ev.id,
+        ev.name,
+        bp.name as body_part,
+        eq.name as equipment,
+        true as is_custom,
+        e.id as parent_exercise_id,
+        e.name as parent_exercise_name
+      FROM public.exercise_variation ev
+      INNER JOIN exercise e ON e.id = ev.exercise_id
+      INNER JOIN body_part bp ON bp.id = e.body_part_id
+      INNER JOIN equipment eq ON eq.id = e.equipment_id
+      WHERE ev.id = $1 AND
+        ev.user_id = $2
+    `;
+    const params = [exerciseVariationId, userId];
+
+    try {
+      const { queryResult } = await this.db.queryV2<ExerciseModel>(
+        query,
+        params,
+      );
+
+      if (!queryResult.length) {
+        return null;
+      }
+
+      return queryResult[0];
+    } catch (e) {
+      throw this.handleError(e, queryName);
+    }
+  }
+
+  public async getExerciseVariationWorkoutHistory(
+    exerciseVariationId: string,
+    userId: string,
+  ): Promise<ExerciseWorkoutHistoryModel[]> {
+    const queryName = 'getExerciseVariationWorkoutHistory';
+    const query = `
+          SELECT
+            w.id,
+            w.name,
+            w.created_at,
+            w.duration,
+            json_agg(
+              json_build_object(
+                'id', ws.id,
+                'reps', ws.reps,
+                'weight', ws.weight,
+                'rpe', ws.rpe
+              )
+            ORDER BY ws.order) as sets
+          FROM workout as w
+          LEFT JOIN workout_exercise we
+            ON we.workout_id = w.id
+          LEFT JOIN workout_set ws
+            ON ws.workout_exercise_id = we.id
+          LEFT JOIN exercise_variation ev
+            ON ev.id = we.exercise_variation_id
+          WHERE ev.id = $1 AND
+            w.user_id = $2
+          GROUP BY w.id, w.name, w.created_at
+          ORDER BY w.created_at DESC
+        `;
+    const params = [exerciseVariationId, userId];
+
+    try {
+      const { queryResult } =
+        await this.db.queryV2<ExerciseWorkoutHistoryModel>(query, params);
+
+      return queryResult;
     } catch (e) {
       throw this.handleError(e, queryName);
     }
