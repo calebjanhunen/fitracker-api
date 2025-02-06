e2ce07dcf71ba62af76e1d7b50979a3f5a2a7e96
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-06 12:09:43 -0500

    Enhance ExerciseNotFoundException to handle variations in error messages

--- src/modules/exercises/internal-errors/exercise-not-found.exception.ts
+++ src/modules/exercises/internal-errors/exercise-not-found.exception.ts
@@ -1,7 +1,9 @@
 import { ResourceNotFoundException } from 'src/common/internal-exceptions/resource-not-found.exception';
 
 export class ExerciseNotFoundException extends ResourceNotFoundException {
-  constructor(id: string) {
-    super(`Exercise not found with id: ${id}`);
+  constructor(id: string, isVariation?: boolean) {
+    super(
+      `Exercise${isVariation ? ' variation' : ''} not found with id: ${id}`,
+    );
   }
 }
